1b6cd5d5028276f145560155af82c9487c7b7ee9
Amna Mubarak <amnamubarakk111@gmail.com>
2023-10-07 12:54:42 +0500

    navbar modified

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -120,12 +120,14 @@ const Header: React.FC<HeaderProps> = () => {
                     : 'bg-blue-900'
             } p-4 lg:p-6 transition-all duration-300 ease-in-out text-white`}
         >
-            <div className="container mx-auto flex justify-between  'bg-blue-900'">
-                <Link href="/" legacyBehavior>
-                    <h1 className="text-2xl font-semibold cursor-pointer">LOGO</h1>
-                </Link>
+            <div className="container mx-auto flex justify-between bg-blue-900 items-center h-16">
+            <Link href="/" legacyBehavior className='py-3'>
+                        <a>
+                            <img src="/synnc.png" alt="Logo" className= "w-32 h-32" /> {/* Replace with your logo image path */}
+                        </a>
+                    </Link>
                 <div className="text-center flex-grow px-10"> {/* Use flex-grow to push items to the center */}
-                    <ul className="lg:flex lg:space-x-6">
+                    <ul className="lg:flex lg:space-x-6 text-center">
                         <li>
                             <Link href="/" legacyBehavior>
                                 <a className="hover:text-gray-200 cursor-pointer">Home</a>
@@ -168,6 +170,33 @@ const Header: React.FC<HeaderProps> = () => {
                         </svg>
                     </button>
                     {/* Add additional options here */}
+                    <div className="hidden lg:flex items-center space-x-4">
+                        {/* <a href="/login" className="hover:text-gray-200 cursor-pointer border border-gray-300 px-3 py-2 rounded">Login</a> Added border classes */}
+                        <a href="/login" className="hover:text-gray-200 cursor-pointer border border-gray-300 px-6 py-3 rounded-full">Sign In</a> {/* Added border classes */}
+                                  
+              <button className=" text-white rounded-full py-3 px-6 border border-gray-300 font-semibold hover:bg-blue-600">
+                Request a demo
+              </button>
+     
+                    </div>
+                    <div className="text-gray-300 cursor-pointer">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="w-6 h-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M21 21l-5.2-5.2"
+                                />
+                                <circle cx="10" cy="10" r="8" />
+                            </svg>
+                        </div>
+
                 </div>
             </div>
         </nav>

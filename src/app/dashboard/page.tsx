993134613e0628722248fe6be74c2955d3c28488
Ehtesham Zafar <ehteshamzafar2002@gmail.com>
2023-10-10 21:35:07 +0500

    login/signup implemented completely

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+
+'use client'
 import React from "react";
 import axios from "axios";
 import { Backend_URL } from "@/lib/Constants";
@@ -7,10 +9,12 @@ const DashboardPage = () => {
   const verifyToken = async () => {
     try {
       const session = await getSession();
+      // console.log(session);
       if (!session || !session?.user || !session?.user.data) {
         throw new Error("Invalid session");
       }
       const jwt: string = session?.user.data.jwt;
+      console.log(jwt);
       const result = await axios.get(Backend_URL + "/auth/validateToken", {
         headers: {
           Authorization: `Bearer ${jwt}`,
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 // Import your DemoPage component
 
 

83771b42a1c9dd65e140634f3dc40b6298e716ef
Amna Mubarak <amnamubarakk111@gmail.com>
2023-10-18 21:34:10 +0500

    error resolved

--- src/components/chart1.tsx
+++ src/components/chart1.tsx
@@ -1,77 +1,75 @@
 import dynamic from 'next/dynamic';
 import React from 'react'
 const ApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
-import { useEffect } from 'react';
+
 
 const Chart1 = () => {
-    useEffect(() => {
-        const getChartOptions = () => {
+    const getChartOptions = () => {
         
-            window.addEventListener("load", function () {
-                const getChartOptions = () => {
-                    return {
-                        series: [52.8, 26.8, 20.4],
-                        colors: ["#1C64F2", "#16BDCA", "#9061F9"],
-                        chart: {
-                            height: 420,
-                            width: "100%",
-                            type: "pie",
-                        },
-                        stroke: {
-                            colors: ["white"],
-                            lineCap: "",
-                        },
-                        plotOptions: {
-                            pie: {
-                                labels: {
-                                    show: true,
-                                },
-                                size: "100%",
-                                dataLabels: {
-                                    offset: -25
-                                }
+        window.addEventListener("load", function () {
+            const getChartOptions = () => {
+                return {
+                    series: [52.8, 26.8, 20.4],
+                    colors: ["#1C64F2", "#16BDCA", "#9061F9"],
+                    chart: {
+                        height: 420,
+                        width: "100%",
+                        type: "pie",
+                    },
+                    stroke: {
+                        colors: ["white"],
+                        lineCap: "",
+                    },
+                    plotOptions: {
+                        pie: {
+                            labels: {
+                                show: true,
                             },
+                            size: "100%",
+                            dataLabels: {
+                                offset: -25
+                            }
                         },
-                        labels: ["Direct", "Organic search", "Referrals"],
-                        dataLabels: {
-                            enabled: true,
-                            style: {
-                                fontFamily: "Inter, sans-serif",
-                            },
-                        },
-                        legend: {
-                            position: "bottom",
+                    },
+                    labels: ["Direct", "Organic search", "Referrals"],
+                    dataLabels: {
+                        enabled: true,
+                        style: {
                             fontFamily: "Inter, sans-serif",
                         },
-                        yaxis: {
-                            labels: {
-                                formatter: function (value : string) {
-                                    return value + "%"
-                                },
+                    },
+                    legend: {
+                        position: "bottom",
+                        fontFamily: "Inter, sans-serif",
+                    },
+                    yaxis: {
+                        labels: {
+                            formatter: function (value : string) {
+                                return value + "%"
                             },
                         },
-                        xaxis: {
-                            labels: {
-                                formatter: function (value: string) {
-                                    return value + "%"
-                                },
-                            },
-                            axisTicks: {
-                                show: false,
-                            },
-                            axisBorder: {
-                                show: false,
+                    },
+                    xaxis: {
+                        labels: {
+                            formatter: function (value: string) {
+                                return value + "%"
                             },
                         },
-                    }
+                        axisTicks: {
+                            show: false,
+                        },
+                        axisBorder: {
+                            show: false,
+                        },
+                    },
                 }
-    
-                  if (document.getElementById("pie-chart") && typeof ApexCharts !== 'undefined') {
-                    const chart = new ApexCharts(document.getElementById("pie-chart"), getChartOptions());
-                    chart.render();
-                  }
-            });
-   
+            }
+
+              if (document.getElementById("pie-chart") && typeof ApexCharts !== 'undefined') {
+                const chart = new ApexCharts(document.getElementById("pie-chart"), getChartOptions());
+                chart.render();
+              }
+        });
     };
 
     return (

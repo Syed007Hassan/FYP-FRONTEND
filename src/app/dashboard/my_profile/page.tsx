93bde6055e80d0883375c842b5164edae82f9098
Amna Mubarak <amnamubarakk111@gmail.com>
2023-10-22 20:22:13 +0500

    ui modification

--- src/app/dashboard/my_profile/page.tsx
+++ src/app/dashboard/my_profile/page.tsx
@@ -81,64 +81,72 @@ const Page = () => {
         <div className="pt-6 pr-20 pl-10 pb-16">
           <div className="pr-20 pl-20">
             <h1 className=" text-blue-500 mb-4">Syncflow recruitment</h1>
-
+            <h1 className="text-4xl text-blue-900 pt-20">My Profile</h1>
             {passwordMatch === false && (
               <Alert severity="error">Passwords do not match</Alert>
             )}
             {success && (
               <Alert severity="success">Profile updated successfully</Alert>
             )}
-            {/* //create containerfor form */}
-            <h1 className="text-4xl text-blue-900 pt-20">My Profile</h1>
-            <div className="md:container mb-3 flex space-x-6 justify-center">
-
-              <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-
-                <div className="mb-3 flex space-x-3">
-                  <div>
-                    <label htmlFor="f_name" className="block text-sm font-medium text-gray-900 dark:text-white">First name</label>
+            <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div className="grid grid-rows-1 grid-flow-col">
+                  <div className="pr-4">
+                    <label htmlFor="firstName" className="block text-sm font-medium text-gray-900 dark:text-white">First name</label>
                     <input type="text"
-                      id="f_name"
-                      className="w-1/2 min-w-fit border rounded p-2 transition duration-300 ease-in-out hover:bg-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-opacity-50 hover:placeholder-opacity-75"
+                      id="firstName"
+                      className="w-full min-w-fit border rounded p-2 transition duration-300 ease-in-out hover:bg-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-opacity-50 hover:placeholder-opacity-75"
                       placeholder="John"
                       onChange={(e) => setFirstName(e.target.value)}
                       required />
                   </div>
-                  <div>
-                    <label htmlFor="l_name" className="block text-sm font-medium text-gray-900 dark:text-white">Last name</label>
+                  <div className="pl-4">
+                    <label htmlFor="lastName" className="block text-sm font-medium text-gray-900 dark:text-white">Last name</label>
                     <input type="text"
-                      id="l_name"
-                      className="w-1/2  min-w-fit border rounded p-2 transition duration-300 ease-in-out hover:bg-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-opacity-50 hover:placeholder-opacity-75"
+                      id="lastName"
+                      className="w-full  min-w-fit border rounded p-2 transition duration-300 ease-in-out hover:bg-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-opacity-50 hover:placeholder-opacity-75"
                       placeholder="Doe"
                       onChange={(e) => setLastName(e.target.value)}
                       required />
 
                   </div>
-
                 </div>
+                <div className="grid grid-rows-1 grid-flow-col pt-10 pb-10">
+                  <div className="pr-4">
+                    <label htmlFor="designation" className="block mt-2 text-sm font-medium text-gray-900 dark:text-white">Designation</label>
+                    <input type="text"
+                      id="designation"
+                      className="w-full min-w-fit border rounded p-2 transition duration-300 ease-in-out hover:bg-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-opacity-50 hover:placeholder-opacity-75"
+                      onChange={(e) => setDesignation(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="pl-4 pt-2">
+                    <label htmlFor="phone" className="block text-sm font-medium text-gray-900 dark:text-white">Phone Number </label>
+                    <input
+                      type="phone"
+                      id="phone"
+                      className=" w-full min-w-fit  border rounded p-2 transition duration-300 ease-in-out hover:bg-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-opacity-50 hover:placeholder-opacity-75"
+                      onChange={(e) => setPhone(e.target.value)}
+                      required />
 
-                <div className="w-full mb-3">
-                  <label htmlFor="designation" className="block mt-2 text-sm font-medium text-gray-900 dark:text-white">Designation</label>
-                  <input type="text"
-                    id="designation"
-                    className="w-full border rounded p-2 transition duration-300 ease-in-out hover:bg-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-opacity-50 hover:placeholder-opacity-75"
-                    onChange={(e) => setDesignation(e.target.value)}
-                    required />
+                  </div>
                 </div>
-
-                <div className="mb-3">
-                  <label htmlFor="phone" className="   block text-sm font-medium text-gray-900 dark:text-white">Phone number</label>
-                  <input type="phone"
-                    id="phone"
-                    className="w-full border rounded p-2 transition duration-300 ease-in-out hover:bg-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-opacity-50 hover:placeholder-opacity-75"
-                    placeholder="123-45-678"
-                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                    onChange={(e) => setPhone(e.target.value)}
-                    required />
-                </div>
-
-
-                <div className="mb-3">
+                <div>
+                  {/* <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    // autoComplete="email"
+                    variant="outlined"
+                    size="small"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    InputProps={{ readOnly: true }}
+                    title="This field is read-only"
+                  /> */}
                   <label htmlFor="email" className="block text-sm font-medium text-gray-900 dark:text-white">Email address</label>
                   <input
                     type="email"
@@ -148,64 +156,76 @@ const Page = () => {
                     onChange={(e) => setEmail(e.target.value)}
                     required />
                 </div>
-
-                <div className="mb-3 flex space-x-3">
-                  <div>
+                <div className="grid grid-rows-1 grid-flow-col pt-10">
+                  <div className="pr-4">
+                    {/* <TextField
+                      required
+                      fullWidth
+                      id="password"
+                      label="Password"
+                      name="password"
+                      autoComplete="new-password"
+                      variant="outlined"
+                      size="small"
+                      onChange={(e) => setPassword(e.target.value)} */}
                     <label htmlFor="password" className="block text-sm font-medium text-gray-900 dark:text-white">Password</label>
                     <input
                       type="password"
                       id="password"
-                      className="w-1/2 min-w-fit border rounded p-2 transition duration-300 ease-in-out hover:bg-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-opacity-50 hover:placeholder-opacity-75"
+                      className="w-full min-w-fit border rounded p-2 transition duration-300 ease-in-out hover:bg-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-opacity-50 hover:placeholder-opacity-75"
                       onChange={(e) => setPassword(e.target.value)}
                       required />
                   </div>
-
-                  <div>
+                  <div className="pl-4">
+                    {/* <TextField
+                      required
+                      fullWidth
+                      id="password"
+                      label="Repeat Password"
+                      name="password"
+                      autoComplete="new-password"
+                      variant="outlined"
+                      size="small"
+                      onChange={(e) => setRepeatPassword(e.target.value)}
+                    /> */}
                     <label htmlFor="repeat_password" className="block text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
                     <input
                       type="password"
-                      id="repeat_password"
-                      className="w-1/2 min-w-fit border rounded p-2 transition duration-300 ease-in-out hover:bg-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-opacity-50 hover:placeholder-opacity-75"
+                      id="password"
+                      className="w-full min-w-fit border rounded p-2 transition duration-300 ease-in-out hover:bg-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-opacity-50 hover:placeholder-opacity-75"
                       onChange={(e) => setRepeatPassword(e.target.value)}
                       required />
                   </div>
                 </div>
-
-                <div className="grid grid-rows-1 grid-flow-col pt-4">
-                  <div className="flex justify-end">
-                    <button
-                      type="submit"
-                      className=" flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                    >
-                      Update
-                    </button>
-                  </div>
-                </div>
-              </form >
-
-
-              <div
-                className="pl-10 pb-6 pr-10 flex"
-                style={{ width: "650px", height: "630px" }}
-              >
-                <Image
-                  src="/landing-pic.png"
-                  alt="Picture of the author"
-                  width={500}
-                  height={500}
-                  className="object-cover w-full h-full"
-                  priority
-                />
               </div>
-
-
-            </div>
-
+              <div className="grid grid-rows-1 grid-flow-col pt-4">
+                <div className="flex justify-end">
+                  <button
+                    type="submit"
+                    className=" flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                  >
+                    Update
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
+        </div>
+        <div
+          className="pl-10 pb-6 pr-10 flex"
+          style={{ width: "650px", height: "630px" }}
+        >
+          <Image
+            src="/landing-pic.png"
+            alt="Picture of the author"
+            width={500}
+            height={500}
+            className="object-cover w-full h-full"
+            priority
+          />
         </div>
       </div>
     </div>
-
   );
 };
 

648408c40321377d8f08ef35b8cf361e8c15f9c0
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-11-02 22:08:18 +0500

    width and height added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="flex flex-col justify-center items-center px-6 mx-auto h-screen xl:px-0 dark:bg-gray-900">
       <div className="block md:max-w-lg">
-        <Image src="/404.svg" alt="astronaut image" />
+        <Image width={700} height={700} src="/404.svg" alt="astronaut image" />
       </div>
       <div className="text-center xl:max-w-4xl">
         <h1 className="mb-3 text-2xl font-bold leading-tight text-gray-900 sm:text-4xl lg:text-5xl dark:text-white">

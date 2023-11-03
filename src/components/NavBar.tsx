3410c87da4554f6b107670f346fcbbfdacbe4858
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-11-04 01:38:36 +0500

    FaIcons adjusted

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,21 @@
 "use client";
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { FaUser, FaUserPlus, FaBuilding, FaBriefcase } from "react-icons/fa";
 import Sidebar from "./Sidebar";
 import Image from "next/image";
-import { FaBars, FaTimes } from "react-icons/fa";
+import {
+  FaUser,
+  FaUserPlus,
+  FaBuilding,
+  FaBriefcase,
+  FaBars,
+  FaTimes,
+  FaSearch,
+} from "react-icons/fa";
 
-interface HeaderProps { }
+interface HeaderProps {}
 
 const NavBar: React.FC<HeaderProps> = () => {
-
   const [sticky, setSticky] = useState<boolean>(false);
   const [isMobileView, setIsMobileView] = useState<boolean>(false);
   const [dropdownVisible, setDropdownVisible] = useState<boolean>(false);
@@ -50,71 +56,61 @@ const NavBar: React.FC<HeaderProps> = () => {
           </div>
         </div>
 
-        <div className="flex space-x-8">
-          <div className="flex items-center">
-            <div className="relative" onClick={toggleDropdown}>
-              <FaUser className="space-x-4" size={22} />
-              {dropdownVisible && (
-                <div className="absolute mt-2 z-auto top-0 w-48 bg-white border rounded-lg shadow-md">
-                  <ul>
-                    <li>
-                      <a
-                        href="/dashboard/my_profile"
-                        className="sticky block px-4 py-2 hover:bg-gray-200 text-blue-500"
-                      >
-                        Your Profile
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="/api/auth/signout"
-                        className="block px-4 py-2 hover:bg-gray-200 text-blue-500"
-                      >
-                        Logout
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              )}
+        <div className="flex space-x-8 items-center">
+          <div
+            className="relative flex items-center justify-center w-8 h-8"
+            onMouseEnter={() => setDropdownVisible(true)}
+            onMouseLeave={() => setDropdownVisible(false)}
+          >
+            <FaUser size={23} />
+            {dropdownVisible && (
+              <div className="absolute mt-7 ml-40 z-50 top-0 w-48 bg-white border rounded-lg shadow-md">
+                <ul>
+                  <li>
+                    <a
+                      href="/dashboard/my_profile"
+                      className="sticky block px-4 py-2 hover:bg-gray-200 text-blue-500"
+                    >
+                      Your Profile
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/api/auth/signout"
+                      className="block px-4 py-2 hover:bg-gray-200 text-blue-500"
+                    >
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+          <Link href="/dashboard/addemployee">
+            <div className="flex items-center justify-center w-8 h-8">
+              <button title="Add Employee">
+                <FaUserPlus size={25} />
+              </button>
             </div>
-          </div>
-          <div className="flex items-center">
-            <Link href="/dashboard/addemployee">
-              <button>
-                <FaUserPlus className="space-x-4 blue-icon" size={25} />
+          </Link>
+          <Link href="/dashboard/company-profile">
+            <div className="flex items-center justify-center w-8 h-8">
+              <button title="Company Profile">
+                <FaBuilding size={25} />
               </button>
-            </Link>
-          </div>
-          <div className="flex items-center">
-            <Link href="/dashboard/company_profile">
-              <button>
-                <FaBuilding className="space-x-4 " size={25} />
+            </div>
+          </Link>
+          <Link href="#">
+            <div className="flex items-center justify-center w-8 h-8">
+              <button title="Briefcase">
+                <FaBriefcase size={25} />
               </button>
-            </Link>
-          </div>
-          <div className="flex items-center">
-            <Link href="#">
-              <button>
-                <FaBriefcase className="material-icons" size={25} />
-              </button>
-            </Link>
-          </div>
-          <div className="text-gray-300 cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-10"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-5.2-5.2"
-              />
-              <circle cx="10" cy="10" r="8" />
-            </svg>
+            </div>
+          </Link>
+          <div className="flex items-center justify-center w-8 h-8 cursor-pointer">
+            <button title="Search">
+              <FaSearch size={25} />
+            </button>
           </div>
         </div>
       </div>

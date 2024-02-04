326b33e283264972042a6d965ec55e39ddfc5f46
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2024-02-04 21:42:34 +0500

    useEffect for geolocation

---
+++ src/lib/extra.ts
@@ -0,0 +1,18 @@
+// const [location, setLocation] = useState(null);
+
+// const getCurrentLocation = () => {
+//   setLocation({});
+// };
+
+// useEffect(() => {
+//   if (location !== null) {
+//     navigator.geolocation.getCurrentPosition((pos) => {
+//       const { latitude, longitude } = pos.coords;
+//       console.log(latitude, longitude);
+//       // const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
+//       // fetch(url)
+//       //   .then((res) => res.json())
+//       //   .then((data) => setAdd(data.address));
+//     });
+//   }
+// }, [location]);

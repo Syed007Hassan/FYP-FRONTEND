e19ead11f6db0ccc12883926581367960e126cd6
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2024-02-20 20:29:00 +0500

    Modals of profile page (#145)
    * redux implemented for update page (cont.)
    
    * .
    
    * update page integration done ig
    
    * modified reduc logic for updateApplicantDetails
    
    * profile page modified,edit modals added
    
    ---------
    
    Co-authored-by: amnaridewithshare <amna@ridewithshare.com>
    Co-authored-by: Amna Mubarak <amnamubarakk111@gmail.com>
    Co-authored-by: Ehtesham Zafar <ehteshamzafar2002@gmail.com>

---
+++ src/redux/services/Applicant/applicantSlice.ts
@@ -0,0 +1,33 @@
+// updateSlice.tsx
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { updateApplicantDetails, resetSuccess } from "./applicantAction";
+
+const applicantSlice = createSlice({
+  name: "update",
+  initialState: {
+    job: null,
+    loading: false,
+    error: null,
+    success: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(updateApplicantDetails.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(updateApplicantDetails.fulfilled, (state, action) => {
+      state.loading = false;
+      state.success = true;
+      state.job = action.payload;
+    });
+    builder.addCase(resetSuccess.fulfilled, (state) => {
+      state.success = false;
+    });
+    builder.addDefaultCase((state, action) => {
+      state.success = false;
+    });
+  },
+});
+
+export default applicantSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,22 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./services/auth/authSlice"
+import authReducer from "./services/auth/authSlice";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { userApi } from "./services/Recruiter/recruiterAction";
 import { companyApi } from "./services/Company/companyAction";
 import { chatApi } from "./services/chat/chatAction";
 import { JobApi } from "./services/job/jobAction";
 import { stageApi } from "./services/stage/stageAction";
+import { updateApi } from "./services/Applicant/applicantAction";
 import { assigneeApi } from "./services/assignee/assigneeAction";
 
 import { userReducer } from "./services/Recruiter/recruiterSlice";
 import { employeeReducer } from "./services/Recruiter/recruiterSlice";
 import companyReducer from "./services/Company/companySlice";
 import jobReducer from "./services/job/jobSlice";
+import applicantReducer from "./services/Applicant/applicantSlice";
 import stageReducer from "./services/stage/stageSlice";
 import assigneeReducer from "./services/assignee/assigneeSlice";
 
-import sidebarReducer from './features/sidebarStateAction';
-
+import sidebarReducer from "./features/sidebarStateAction";
 export const store = configureStore({
   reducer: {
     authReducer,
@@ -26,23 +27,25 @@ export const store = configureStore({
     jobReducer,
     stageReducer,
     assigneeReducer,
+    applicantReducer,
     [userApi.reducerPath]: userApi.reducer,
     [companyApi.reducerPath]: companyApi.reducer,
     [chatApi.reducerPath]: chatApi.reducer,
     [JobApi.reducerPath]: JobApi.reducer,
+    [updateApi.reducerPath]: updateApi.reducer,
     [stageApi.reducerPath]: stageApi.reducer,
     [assigneeApi.reducerPath]: assigneeApi.reducer,
     sidebar: sidebarReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware()
-  .concat(userApi.middleware)
-  .concat(companyApi.middleware)
-  .concat(chatApi.middleware)
-  .concat(JobApi.middleware)
-  .concat(stageApi.middleware)
-  .concat(assigneeApi.middleware),
+    getDefaultMiddleware()
+      .concat(userApi.middleware)
+      .concat(companyApi.middleware)
+      .concat(chatApi.middleware)
+      .concat(JobApi.middleware)
+      .concat(stageApi.middleware)
+      .concat(assigneeApi.middleware),
 });
 
 setupListeners(store.dispatch);

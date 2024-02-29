26a73734aa6054a8e8c0fb6f3a6a792f13ab3ede
Ehtesham Zafar <ehteshamzafar2002@gmail.com>
2024-02-29 11:59:46 +0500

    Fix CORS headers in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -109,10 +109,10 @@ export function middleware(req: NextRequest) {
       // add the CORS headers to the response
       res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
       res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-      // res.headers.append(
-      //     'Access-Control-Allow-Headers',
-      //     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-      // )
+      res.headers.append(
+          'Access-Control-Allow-Headers',
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      )
 
   return res;
 }

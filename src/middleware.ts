2a65ec3146f23bf2d2d70b1637cff06679d00f0f
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2024-02-29 16:28:11 +0500

    Refactor CORS headers in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -104,15 +104,17 @@ export function middleware(req: NextRequest) {
     return NextResponse.next();
   }
 
-    const res: NextResponse = NextResponse.next();
+  //   const res: NextResponse = NextResponse.next();
 
-      // add the CORS headers to the response
-      res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-      res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-      res.headers.append(
-          'Access-Control-Allow-Headers',
-          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-      )
+  //     // add the CORS headers to the response
+  //     res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  //     res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  //     res.headers.append(
+  //         'Access-Control-Allow-Headers',
+  //         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  //     )
 
-  return res;
+  // return res;
+
+  return NextResponse.next();
 }

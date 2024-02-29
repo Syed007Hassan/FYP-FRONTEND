23180a9f78d021b4ca8b0c41342ef69d1c119b97
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2024-02-29 19:21:36 +0500

    Update Backend_URL constant

--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -1,6 +1,7 @@
 //env variables also added in vercel project settings
-export const Backend_URL = process.env
-  .NEXT_PUBLIC_BACKEND_NEST_URL_PROD as string;
+// export const Backend_URL = process.env
+//   .NEXT_PUBLIC_BACKEND_NEST_URL_PROD as string;
+export const Backend_URL = "http://fypnest.eastus.cloudapp.azure.com:5000/api";
 export const FLASK_URL = process.env
   .NEXT_PUBLIC_BACKEND_FLASK_URL_DEV as string;
 

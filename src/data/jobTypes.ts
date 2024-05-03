5a4f6e01703e3cdeabb42dcc338658bae7dca893
amnaridewithshare <amna@ridewithshare.com>
2024-05-03 13:56:02 -0400

    dropdown added for designation

--- src/data/jobTypes.ts
+++ src/data/jobTypes.ts
@@ -8,5 +8,11 @@ const jobTypes = [
   "Remote",
 ];
 export const qualifications = ["Intermediate", "Bachelors", "Masters", "PhD"];
+// export const designation = ["Lead HR","Junior HR","HR Officer"]
+export const designations = [
+  { value: 'designation1', label: 'Lead HR' },
+  { value: 'designation2', label: 'Juinor HR' },
+  { value: 'designation3', label: 'HR Officer' },
+];
 
 export default jobTypes;

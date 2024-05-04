cb240cde0eb12f7c6f0a7336c7cc83cf8c8c538a
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2024-05-04 23:10:38 +0500

    feat: Update backend URL in Constants.ts
    The backend URL in the Constants.ts file was updated from the production URL to the development URL. This change ensures that the application uses the correct backend URL during development.

--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -1,8 +1,6 @@
 //env variables also added in vercel project settings
-// export const Backend_URL = process.env
-//   .NEXT_PUBLIC_BACKEND_NEST_URL_PROD as string;
 export const Backend_URL = process.env
-  .NEXT_PUBLIC_BACKEND_NEST_URL_PROD as string;
+  .NEXT_PUBLIC_BACKEND_NEST_URL_DEV as string;
 export const FLASK_URL = process.env
   .NEXT_PUBLIC_BACKEND_FLASK_URL_PROD as string;
 
@@ -11,7 +9,6 @@ export function parseJwt(jwt: string): any {
   if (parts.length < 2) {
     return null; // or throw an error
   }
-
   const base64Url = parts[1];
   const base64 = base64Url.replace("-", "+").replace("_", "/");
   return JSON.parse(Buffer.from(base64, "base64").toString("binary"));
